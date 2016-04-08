3a1f5039fdaeb25a28771a10c02b4827f9268254
victor Lopez <vic_820@hotmail.com>
2016-04-07 23:34:30 -0500

    Subidos los archivos

---
+++ script.js
@@ -0,0 +1,7 @@
+/* Veamos que tal se ve si cambio el color */
+
+debugger;
+var parrafo = document.getElementById("cambio");
+parrafo.style.color = "red";
+parrafo.innerHTML = "Y asi de color rojo!";
+
